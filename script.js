2f5baeb7be36836549600e2cc1db324d9a085d85
suhita4 <63366417+suhita4@users.noreply.github.com>
2022-02-17 17:20:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+var x = 1;
+function showOrHide1() {
+  if (x == 1) {
+    document.getElementById("answer1").style.display = "flex";
+    document.getElementById("plus1").style.transform = 'rotate(45deg)';
+
+    return x = 0;
+  }
+  else {
+    document.getElementById("answer1").style.display = "none";
+    document.getElementById("plus1").style.transform = 'rotate(90deg)';
+
+    return x = 1;
+  }
+}
+
+function showOrHide2() {
+  if (x == 1) {
+    document.getElementById("answer2").style.display = "flex";
+    document.getElementById("plus2").style.transform = 'rotate(45deg)';
+
+    return x = 0;
+  }
+  else {
+    document.getElementById("answer2").style.display = "none";
+    document.getElementById("plus2").style.transform = 'rotate(90deg)';
+
+    return x = 1;
+  }
+}
+
+function showOrHide3() {
+  if (x == 1) {
+    document.getElementById("answer3").style.display = "flex";
+    document.getElementById("plus3").style.transform = 'rotate(45deg)';
+
+    return x = 0;
+  }
+  else {
+    document.getElementById("answer3").style.display = "none";
+    document.getElementById("plus3").style.transform = 'rotate(90deg)';
+
+    return x = 1;
+  }
+}
+
+function showOrHide4() {
+  if (x == 1) {
+    document.getElementById("answer4").style.display = "flex";
+    document.getElementById("plus4").style.transform = 'rotate(45deg)';
+
+    return x = 0;
+  }
+  else {
+    document.getElementById("answer4").style.display = "none";
+    document.getElementById("plus4").style.transform = 'rotate(90deg)';
+
+    return x = 1;
+  }
+}
+
+function showOrHide5() {
+  if (x == 1) {
+    document.getElementById("answer5").style.display = "flex";
+    document.getElementById("plus5").style.transform = 'rotate(45deg)';
+
+    return x = 0;
+  }
+  else {
+    document.getElementById("answer5").style.display = "none";
+    document.getElementById("plus5").style.transform = 'rotate(90deg)';
+
+    return x = 1;
+  }
+}
+
